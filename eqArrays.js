8ce19faca425e6e4c2503faa791f998a88ecf2b3
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-10-23 15:38:35 -0600

    Compare Arrays

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌: ${actual} !==  ${expected}`);
+  }
+};
+
+const eqArrays = function(arrOne,arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  } else {
+    for (let i = 0; i <= arrOne.length; i++) {
+      if (arrOne[i] !== arrTwo[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
