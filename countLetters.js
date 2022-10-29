6d001f265448de10231e45122d318d03768dc45f
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-10-29 16:27:13 -0600

    Count the Letters in a String

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌: ${actual} !==  ${expected}`);
+  }
+};
+
+
+const countLetters = function(strings) {
+  const results = {}
+  for(const string of strings){ 
+    if(string !== " ")
+      if(results[string]){
+      results[string] += 1;
+    } else{
+      results[string] = 1;
+    }
+  }
+  
+  return results;
+}
+
+console.log(countLetters("lighthouse in the house"))
