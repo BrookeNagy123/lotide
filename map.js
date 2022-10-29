5cea71cb5fef728a2e6d2fd10e4ae4a3791f8420
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-10-29 17:06:19 -0600

    My own map

---
+++ map.js
@@ -0,0 +1,41 @@
+
+
+const map = function(array, callback) {
+  const results = [];
+  for(let item of array){
+    results.push(callback(item));
+  }
+  return results;
+}
+
+const eqArrays = function(arrOne,arrTwo) {
+  if (arrOne.length !== arrTwo.length) {
+    return false;
+  } else {
+    for (let i = 0; i <= arrOne.length; i++) {
+      if (arrOne[i] !== arrTwo[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(arrOne, arrTwo) {
+  let arraysEqual = eqArrays(arrOne, arrTwo);
+  if (arraysEqual === true) {
+    console.log(`✅: ${arrOne} === ${arrTwo}`);
+  } else {
+    console.log(`❌: ${arrOne} !==  ${arrTwo}`);
+  }
+};
+
+/* Further Test Cases Needed
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
+*/ 
+
+
+
