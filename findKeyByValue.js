6cd49d7173ab93fa20b2c67a789b9e8cd4ddd99a
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-05 18:18:47 -0600

    Find Key in Object by Value

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌: ${actual} !==  ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function(object, value){
+  return Object.keys(object).find(key => object[key] === value);
+  // Object Keys returns all the keys in the object. We use find to check if any of the keys match the value. 
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+findKeyByValue(bestTVShowsByGenre, "The Wire");
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
