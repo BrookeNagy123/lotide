ef22dd2491694c33e72967e8c539e6fddd065d18
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-06 15:04:07 -0700

    Refactor

--- middle.js
+++ middle.js
@@ -1,24 +1,5 @@
-const eqArrays = function(arrOne,arrTwo) {
-  if (arrOne.length !== arrTwo.length) {
-    return false;
-  } else {
-    for (let i = 0; i <= arrOne.length; i++) {
-      if (arrOne[i] !== arrTwo[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-
-const assertArraysEqual = function(arrOne, arrTwo) {
-  let arraysEqual = eqArrays(arrOne, arrTwo);
-  if (arraysEqual === true) {
-    console.log(`✅: ${arrOne} === ${arrTwo}`);
-  } else {
-    console.log(`❌: ${arrOne} !==  ${arrTwo}`);
-  }
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(arr){
   let middleOne = [];
@@ -41,15 +22,6 @@ const middle = function(arr){
   return results
 }
 
+module.exports = middle;
 
-/* Test Cases */ 
-
-console.log(middle([1])) // => []
-console.log(middle([1, 2])) // => []
-
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
 
