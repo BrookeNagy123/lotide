058f83cfbb44ac9642c27bd5c88bb029bc0d1e72
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-06 14:39:29 -0700

    Refactor

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌: ${actual} !==  ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
   if (arr.length !== 0) {
@@ -14,7 +8,4 @@ const head = function(arr) {
   }
 };
 
-
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
