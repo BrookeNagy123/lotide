725a566b25c7d10aebc4288cc7135f9e1184e49e
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-07 18:28:58 -0700

    eslint

--- eqObjects.js
+++ eqObjects.js
@@ -21,19 +21,19 @@ const eqArrays = function(arrOne,arrTwo) {
 
 const eqObjects = function(object1, object2) {
   const keys1 = Object.keys(object1);
-  const keys2 = Object.keys(object2)
+  const keys2 = Object.keys(object2);
 
-  if(keys1.length !== keys2.length){
+  if (keys1.length !== keys2.length) {
     return false;
-  } 
-  for(let key of keys1){
+  }
+  for (let key of keys1) {
     const val1 = object1[key];
     const val2 = object2[key];
-    if(Array.isArray(val1) || Array.isArray(val2)){
+    if (Array.isArray(val1) || Array.isArray(val2)) {
       return eqArrays(val1, val2);
-    } else if (val1 !== val2){
+    } else if (val1 !== val2) {
       return false;
-    } 
+    }
   }
   return true;
 };
