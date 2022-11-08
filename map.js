e5cd673a8fd25f4f8bb5dd45fd321cf4f6ff5a8f
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-07 18:28:08 -0700

    eslint

--- map.js
+++ map.js
@@ -2,11 +2,11 @@
 
 const map = function(array, callback) {
   const results = [];
-  for(let item of array){
+  for (let item of array) {
     results.push(callback(item));
   }
   return results;
-}
+};
 
 const eqArrays = function(arrOne,arrTwo) {
   if (arrOne.length !== arrTwo.length) {
@@ -30,12 +30,13 @@ const assertArraysEqual = function(arrOne, arrTwo) {
   }
 };
 
+
 /* Further Test Cases Needed
 const words = ["ground", "control", "to", "major", "tom"];
 const results1 = map(words, word => word[0]);
 console.log(results1);
 
-*/ 
+*/
 
 
 
