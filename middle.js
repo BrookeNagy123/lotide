c7e9e0c13f32eacd3db67a40010e6e82135c354d
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-07 18:27:49 -0700

    eslint

--- middle.js
+++ middle.js
@@ -1,26 +1,26 @@
 const eqArrays = require('./eqArrays');
 const assertArraysEqual = require('./assertArraysEqual');
 
-const middle = function(arr){
+const middle = function(arr) {
   let middleOne = [];
   let middleTwo = [];
   let results = [];
-  for(let i = 0; i < arr.length; i++){
-  if(arr.length <= 2){
-    results = [];
-  } else if(arr.length % 2 === 0){
-    middleOne = Math.floor((arr.length / 2) -1 );
-    middleArrOne = [arr[middleOne]];
-    middleTwo = Math.floor(arr.length  / 2)
-    middleArrTwo = [arr[middleTwo]];
-    results = middleArrOne.concat(middleArrTwo);
-  } else {
-    middleOne = Math.floor(arr.length / 2);
-    results = [arr[middleOne]]
+  for (let i = 0; i < arr.length; i++) {
+    if (arr.length <= 2) {
+      results = [];
+    } else if (arr.length % 2 === 0) {
+      middleOne = Math.floor((arr.length / 2) - 1);
+      let middleArrOne = [arr[middleOne]];
+      middleTwo = Math.floor(arr.length  / 2);
+      let middleArrTwo = [arr[middleTwo]];
+      results = middleArrOne.concat(middleArrTwo);
+    } else {
+      middleOne = Math.floor(arr.length / 2);
+      results = [arr[middleOne]];
     }
   }
-  return results
-}
+  return results;
+};
 
 module.exports = middle;
 
