2b81283ca3d6abe52e17003bad35918e0028948a
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-09 17:11:47 -0700

    Cleaned up comments

--- eqArrays.js
+++ eqArrays.js
@@ -1,9 +1,13 @@
 const assertEqual = require('./assertEqual');
 
+// A function that takes in 2 arrays and returns true if the arrays are equal and false otherwise.
+
 const eqArrays = function(arrOne,arrTwo) {
+  //If the array lengths do not match return false.
   if (arrOne.length !== arrTwo.length) {
     return false;
   } else {
+    //Otherwise if array lengths match loop the array and see of the index for each array matches.
     for (let i = 0; i <= arrOne.length; i++) {
       if (arrOne[i] !== arrTwo[i]) {
         return false;
