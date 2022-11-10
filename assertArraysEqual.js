56b1de23874c3bfd6ec36c67c3aa9798d5a091d9
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-09 17:13:41 -0700

    Cleaned up comments

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,7 +1,10 @@
 const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
 
+// A function that takes in 2 arrays and returns true if they are equal and false otherwise.
+
 const assertArraysEqual = function(arrOne, arrTwo) {
+  // Using eqArrays function.
   let arraysEqual = eqArrays(arrOne, arrTwo);
   if (arraysEqual === true) {
     console.log(`✅: ${arrOne} === ${arrTwo}`);
