681d3cd5a9109cf17d49fac88e6cbfe75713c1d0
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-09 17:09:01 -0700

    Cleaned up comments

--- head.js
+++ head.js
@@ -1,9 +1,13 @@
 const assertEqual = require('./assertEqual');
 
+// A function that takes in an array and returns the first element in the array.
+
 const head = function(arr) {
+  // If the array length does not equal zero return the first index of the array, which is arr[0].
   if (arr.length !== 0) {
     return arr[0];
   } else {
+  // If the array length is equal to zero return undefined.
     return undefined;
   }
 };
