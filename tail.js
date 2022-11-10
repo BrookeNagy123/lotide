08adc731db4d024741ee1d2be1d1fc4922aed62d
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-09 17:08:07 -0700

    Cleaned up comments

--- tail.js
+++ tail.js
@@ -1,6 +1,9 @@
 const assertEqual = require('./assertEqual');
 
+// A function that takes in an array and returns everything except the first element of the array.
+
 const tail = function(arr) {
+  //If the array length is greater than 1, create a new array that slices at index 1 and returns the "tail" of the array.
   if (arr.length > 1) {
     let newArr = arr.slice(1);
     return newArr;
