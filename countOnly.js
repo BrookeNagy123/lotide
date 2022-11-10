bc616dcf03e04dc07f6e12e38085c6a90fe064f7
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-09 17:12:17 -0700

    Cleaned up Comments

--- countOnly.js
+++ countOnly.js
@@ -6,9 +6,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// A function that takes in a collection of items and returns a specific subset of those items.
+
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
-
+  // Loop allItems.
   for (const item of allItems) {
     if (itemsToCount[item]) {
       if (results[item]) {
