09cbe87ee7d0932821ac842bc6e7f27a980cbda7
Brooke Nagy <brooke_nagy123@hotmail.com>
2022-11-09 17:12:31 -0700

    Cleaned up comments

--- countLetters.js
+++ countLetters.js
@@ -6,14 +6,19 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// A function that takes in a sentence and returns a count of each of the letters in the sentence.
 
 const countLetters = function(strings) {
   const results = {};
+  // Loop the String.
   for (const string of strings) {
+    //Do not count spaces.
     if (string !== " ") {
       if (results[string]) {
+        //If we have already logged results for results[string] add 1 to the count.
         results[string] += 1;
       } else {
+        //If we have not logged results for results[string] make the count equal to 1.
         results[string] = 1;
       }
     }
